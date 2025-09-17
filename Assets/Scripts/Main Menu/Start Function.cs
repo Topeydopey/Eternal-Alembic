a3ad9684548a50008cb24b2b37472d4600f3c6aa
ChaosTr <nakroth19081990@gmail.com>
2025-09-17 21:03:36 +0700

    Main Menu
    - Add Main Menu scene to Build Profile
    - Add Main Menu arts
    - Add Ouroboros script + Start game script
    - Add animations for Main Menu scene

---
+++ Assets/Scripts/Main Menu/Start Function.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartFunction : MonoBehaviour
+{
+    public GameObject head;
+    public GameObject replace;
+    
+    private bool isTriggered = false;
+    public bool isStart = false;
+
+    private bool inputLocked = true;
+    
+    public FadeScript fadeScript; 
+
+    void Start()
+    {
+        head.SetActive(false);
+        replace.SetActive(true);
+
+        StartCoroutine(DelayInput());
+    }
+
+    void Update()
+    {
+        if (Keyboard.current.anyKey.wasPressedThisFrame && !inputLocked) //if(Input.anyKey)
+        {
+            isStart = true;
+            Debug.Log("Pressed");
+        }
+
+        if (!isTriggered && isStart)
+        {
+            isTriggered = true;
+            StartCoroutine(StartGame());
+            Debug.Log("CHeck");
+        }
+    }
+
+    IEnumerator DelayInput()
+    {
+        yield return new WaitForSeconds(3);
+        inputLocked = false;
+    }
+
+    IEnumerator StartGame()
+    {
+        replace.SetActive(false);
+        head.SetActive(true);
+
+        fadeScript.FadeIn();
+
+        yield return new WaitForSeconds(4);
+
+        SceneManager.LoadScene("Home");
+
+    }
+}
